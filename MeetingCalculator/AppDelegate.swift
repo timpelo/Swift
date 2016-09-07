9078bbc6578bf63f6dd2788e52c47c3f63647141
developer <developer@kc408mac07.tamk.ad>
2016-09-07 13:22:22 +0300

    initial

--- MeetingCalculator/AppDelegate.swift
+++ MeetingCalculator/AppDelegate.swift
@@ -12,10 +12,13 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var meeting : MeetingCostModel?
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        meeting = MeetingCostModel(numberOfParticipants: 10, averageHourSalary: 2500.0, currency: "Euro")
+        meeting!.startMeeting()
         return true
     }
 
@@ -27,6 +30,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        let cost = meeting?.getCurrentCostOfMeeting
+        NSLog("Cost is " + String(cost!))
     }
 
     func applicationWillEnterForeground(application: UIApplication) {
---
+++ MeetingCalculator/MeetingCostModel.swift
@@ -0,0 +1,51 @@
+//
+//  MeetingCostModel.swift
+//  MeetingCalculator
+//
+//  Created by developer on 07/09/16.
+//  Copyright © 2016 developer. All rights reserved.
+//
+
+import Foundation
+
+class MeetingCostModel {
+    var averageHourSalary : Double?
+    var isMeetingOn : Bool = false
+    var numberOfParticipants : Int?
+    var latitude : Double?
+    var longitude : Double?
+    var startMeetingDate : NSDate?
+    var currentcy : String?
+    
+    init(numberOfParticipants : Int, averageHourSalary : Double, currency : String) {
+        self.numberOfParticipants = numberOfParticipants;
+        self.averageHourSalary = averageHourSalary
+        self.currentcy = currency
+    }
+    
+    // initializes startMeetingDate if meeting was off
+    func startMeeting() {
+        NSLog("Meeting started")
+        if(!isMeetingOn) {
+            startMeetingDate = NSDate()
+            isMeetingOn = true
+        }
+    }
+    
+    // returns the current cost of meeting using startMeetingDate, averageHourSalary
+    // and numberOfParticipants
+    var getCurrentCostOfMeeting : Double? {
+        
+        if(isMeetingOn) {
+            let checkpoint = NSDate()
+            let difference = checkpoint.timeIntervalSinceDate(startMeetingDate!)
+            return Double(difference) * averageHourSalary!
+        }
+        return nil
+    }
+    
+    // sets isMeetingOn to false
+    func endMeeting() {
+        isMeetingOn = false
+    }
+}
