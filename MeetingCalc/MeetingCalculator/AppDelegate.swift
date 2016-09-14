496a9622df34d340d6986385ecc4ea29bfc12911
developer <developer@kc408mac07.tamk.ad>
2016-09-14 11:02:22 +0300

    working

--- MeetingCalc/MeetingCalculator/AppDelegate.swift
+++ MeetingCalc/MeetingCalculator/AppDelegate.swift
@@ -14,20 +14,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
 
     var window: UIWindow?
     var meeting : MeetingCostModel?
-    var updateTimer : NSTimer?
-    var locationManager : CLLocationManager?
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Creates and starts meeting.
-        meeting = MeetingCostModel(numberOfParticipants: 10, averageHourSalary: 35.0, currency: "Euro")
-        meeting!.startMeeting()
         
-        // Creates location manager, request permission and starts updating location.
-        locationManager = CLLocationManager()
-        locationManager?.requestWhenInUseAuthorization()
-        locationManager?.delegate = self
-        locationManager?.startUpdatingLocation()
+        // Creates empty meeting and sets it to view controller
+        meeting = MeetingCostModel(numberOfParticipants: 0, averageHourSalary: 0, currency: "€")
+        let viewController : ViewController? = window?.rootViewController as? ViewController
+        
+        if let viewControllerSuccess : ViewController = viewController! {
+            viewControllerSuccess.meeting = meeting;
+        }
 
         return true
     }
@@ -38,7 +35,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
 
     func applicationDidEnterBackground(application: UIApplication) {
         // Invalidates timer.
-        updateTimer?.invalidate()
+        
     }
 
     func applicationWillEnterForeground(application: UIApplication) {
@@ -47,35 +44,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
 
     func applicationDidBecomeActive(application: UIApplication) {
         // Creates new timer to track meeting.
-        NSLog("Registered timer")
-        updateTimer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: #selector(updateMeetingCost), userInfo: nil, repeats: true)
     }
 
     func applicationWillTerminate(application: UIApplication) {
 
     }
     
-    // MARK: My methods
-    func updateMeetingCost() {
-        if let meetingTmp : MeetingCostModel? = meeting {
-            let cost = meetingTmp!.getCurrentCostOfMeeting
-            NSLog("Cost is " + String(cost!))
-        }
-    }
     
-    // MARK: CLLocationManagerDelegate delegate methods
-    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        
-        // Stop updating location.
-        locationManager?.stopUpdatingLocation()
-        
-        // Set coordinates for meeting.
-        let location = locations[0].coordinate
-        meeting?.latitude = location.latitude
-        meeting?.longitude = location.longitude
-        
-        NSLog("Got location at " + String(location.latitude) + ", " + String(location.longitude))
-    }
 
 
 }
--- MeetingCalc/MeetingCalculator/ViewController.swift
+++ MeetingCalc/MeetingCalculator/ViewController.swift
@@ -7,9 +7,21 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+
+    @IBOutlet weak var meetingNameField: UITextField!
+    @IBOutlet weak var meetingMembersField: UITextField!
+    @IBOutlet weak var averageSalaryField: UITextField!
+    @IBOutlet weak var costLabel: UILabel!
+    @IBOutlet weak var startStopButton: UIButton!
+    
+    var meeting : MeetingCostModel?
+    var timer : NSTimer?
+    var locationManager : CLLocationManager?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +32,59 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func startMeeting(sender: AnyObject) {
+        let participantNumber : Int? = Int(meetingMembersField.text!)
+        let averageSalary : Double? = Double(averageSalaryField.text!)
+        
+        if((meeting?.isMeetingOn)!){
+            // Invalidates timer and updates button text.
+            timer?.invalidate()
+            meeting?.isMeetingOn = false;
+            startStopButton.setTitle("Start meeting", forState: UIControlState.Normal)
+            
+        } else if(participantNumber != nil && averageSalary != nil) {
+            // If meeting wasn't on, update details.
+            if let meetingSuccess : MeetingCostModel = meeting {
+                meetingSuccess.averageHourSalary = averageSalary!
+                meetingSuccess.numberOfParticipants = participantNumber!
+                meetingSuccess.startMeeting()
+            }
+            
+            // Sets location manager.
+            locationManager = CLLocationManager()
+            locationManager?.requestWhenInUseAuthorization()
+            locationManager?.delegate = self
+            locationManager?.startUpdatingLocation()
+            timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: #selector(updateMeetingCost), userInfo: nil, repeats: true)
+            
+            // Changes button text to stop.
+            startStopButton.setTitle("Stop meeting", forState: UIControlState.Normal)
+            NSLog("Meeting starts")
+            NSLog("Name: " + meetingNameField.text!)
+            NSLog("Members: " + meetingMembersField.text!)
+            NSLog("Salary: " + averageSalaryField.text!)
+            
+        }
+    }
+    
+    func updateMeetingCost() {
+        var cost : Double = (meeting?.getCurrentCostOfMeeting)!
+        cost = (round(cost*100))/100
+        costLabel.text = String(cost) + (meeting?.currency)!
+    }
+    
+    // MARK: CLLocationManagerDelegate delegate methods
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        // Stop updating location.
+        locationManager?.stopUpdatingLocation()
+        
+        // Set coordinates for meeting.
+        let location = locations[0].coordinate
+        meeting?.latitude = location.latitude
+        meeting?.longitude = location.longitude
+        
+        NSLog("Meeting set to " + String(meeting?.latitude!) + ", " + String(meeting?.longitude!))
+    }
 }
 
